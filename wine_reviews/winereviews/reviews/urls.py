3587dfa7e66a88986b4f5602c42969875fb2a318
Maxim Uvarov <uvarov.max@gmail.com>
2018-04-03 17:28:06 +0300

    Form bug fixed

--- wine_reviews/winereviews/reviews/urls.py
+++ wine_reviews/winereviews/reviews/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url('^wine$', views.wine_list, name='wine_list'),
     # ex: /wine/5/
     url('^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+    url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
